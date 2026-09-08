245d17a47c2e45cb0c5209ee7ffd5d655a56eaaf
unknown <jaydenyeoh08@gmail.com>
2026-09-08 19:52:45 +0800

    Fixed security issues

--- cloud_database_server/settings.py
+++ cloud_database_server/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n9b-n*0t(vm6o+vijobkbhz)4ul)9m!!$0s(g_tmi=_k!s0+$z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
